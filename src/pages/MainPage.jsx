3f6e649efa73eaa8e05395245dbc91bb64589e13
Gabrielly Vitória <gabriellyv1244@gmail.com>
2025-01-29 14:06:13 -0300

    refactoring

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,6 @@
-import AppHeader from "../components/AppHeader/AppHeader"
-import Testando from "../components/Testando/Testando"
-
 function MainPage(){
     return (
         <div>
-            <AppHeader />
-            <Testando/>
         </div>
     )
 }
